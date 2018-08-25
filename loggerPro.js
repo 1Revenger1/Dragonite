2ae04a4457174b56698c5e5a91c6ee0f7837f038
Avery Black <Avery Black@Excaliber>
2018-08-25 10:15:16 -0700

    Added skillpoints, cleaned up loggerpro.

--- loggerPro.js
+++ loggerPro.js
@@ -6,241 +6,309 @@ exports.run = (bot) => {
     var dragonite = `./Dragonite.js`;
 	this.bot = bot;
 
-
 	try{
 		//Color for Join Logs //#4760ff
-		bot.client.on("guildMemberAdd", (member) => {
-			try{
-				let server = bot.servers[member.guild.id];
-				
-				if(server.loggingEnabled != "true" || server.loggingJoin != "true"){
-					return;
-				}
+		bot.client.on("guildMemberAdd", member => guildMemberAdd(member));
+		bot.client.on("guildMemberRemove", member => guildMemberRemove(member));
 
-				var joinEmbed = new Discord.MessageEmbed()
-					.setColor("#4760ff")
-					.setTitle("User joined")
-					.setThumbnail(member.user.displayAvatarURL())
-					.setDescription(`${member} has joined!\n${member.guild.memberCount} users in this guild.`)
-					.setTimestamp(new Date());
-
-
-				if(server.userLogChannel != undefined){
-					server.userLogChannel.send({embed: joinEmbed});
-				} else {
-					server.logChannel.send({embed: joinEmbed});
-				}
-			} catch(err){
-				console.log(err);
-			}
-			
-		});
-
-		bot.client.on("guildMemberRemove", (member) => {
-			try{
-				let server = bot.servers[member.guild.id];
-				
-				if(server.loggingEnabled != "true" || server.loggingJoin != "true"){
-					return;
-				}
-				
-				var joinEmbed = new Discord.MessageEmbed()
-					.setColor("#4760ff")
-					.setTitle("User left")
-					.setThumbnail(member.user.displayAvatarURL())
-					.setDescription(`${member} has left!\n${member.guild.memberCount} users in this guild.`)
-					.setTimestamp(new Date());
-
-
-				if(server.userLogChannel != undefined){
-					server.userLogChannel.send({embed: joinEmbed});
-				} else {
-					server.logChannel.send({embed: joinEmbed});
-				}
-			} catch(err){
-				console.log(err);
-			}
-		});
-
-		bot.client.on("messageDelete", message => {
-			try{
-				if(isDM(message)) return;
-				let server = bot.servers[message.guild.id];
-				if(server.loggingEnabled != "true" || server.loggingMessage != "true"){
-					return;
-				}
-
-				var messageEmbed = new Discord.MessageEmbed()
-					.setColor("#ff4747")
-					.setTitle("Message deleted by " + message.member.displayName)
-					.setThumbnail(message.member.user.displayAvatarURL())
-					.setDescription(`Message content:\n${message.content}`)
-					.setTimestamp(new Date());
-
-
-				server.logChannel.send({embed: messageEmbed});
-			} catch(err){
-				console.log(err);
-			}
-		});
-
-		bot.client.on("messageDeleteBulk", message => {
-			try{
-				if(isDM(message)) return;
-				let server = bot.servers[message.first().guild.id];
-				if(server.loggingEnabled != "true" || server.loggingMessage != "true"){
-					return;
-				}
-
-				var messageEmbed = new Discord.MessageEmbed()
-					.setColor("#ff4747")
-					.setTitle("Messages deleted in bulk")
-					.setDescription(`${message.size} messages deleted in ${message.first().channel}`)
-					.setTimestamp(new Date());
-
-				server.logChannel.send({embed: messageEmbed});
-			} catch(err){
-				console.log(err);
-			}
-		});
+		//Edited/deleted messages
+		bot.client.on("messageDelete", message => messageDelete(message));
+		bot.client.on("messageDeleteBulk", message => messageDeleteBulk(message));
+		bot.client.on("messageUpdate", (oldMessage, newMessage) => messageUpdate(oldMessage, newMessage));
 		
-		bot.client.on("messageUpdate", (oldMessage, newMessage) => {
-			try{
-				if(isDM(oldMessage)) return;
-				var server = null;
-
-				server = bot.servers[oldMessage.guild.id];
-
-				if(server.loggingEnabled != "true" || server.loggingMessage != "true"){
-					return;
-				}
-
-				if(oldMessage.content == newMessage.content){
-					return;
-				}
-
-				var messageEmbed = new Discord.MessageEmbed()
-					.setColor("#ff4747")
-					.setTitle("Messages edited by " + oldMessage.member.displayName)
-					.setThumbnail(oldMessage.member.user.displayAvatarURL())
-					.setDescription(`Old message:\`\`\`\n${oldMessage.content}\`\`\`\nNew message:\`\`\`\n${newMessage.content}\`\`\``)
-					.setTimestamp(new Date());
-
-
-				server.logChannel.send({embed: messageEmbed});
-			} catch(err){
-				console.log(err);
-			}
-		});
-
-		bot.client.on("guildBanAdd", (guild, member) => {
-			try{
-				let server = bot.servers[guild.id];
-				if(server.loggingEnabled != "true" || server.loggingUser != "true"){
-					return;
-				}
-
-				var messageEmbed = new Discord.MessageEmbed()
-					.setColor("#f347ff")
-					.setTitle("User banned")
-					.setThumbnail(member.user.displayAvatarURL())
-					.setDescription(`${member} was banned`)
-					.setTimestamp(new Date());
-					
-
-				server.logChannel.send({embed: messageEmbed});
-			} catch(err){
-				console.log(err);
-			}
-		});
-
-		bot.client.on("guildBanRemove", (guild, member) => {
-			try{
-				let server = bot.servers[guild.id];
-				if(server.loggingEnabled != "true" || server.loggingUser != "true"){
-					return;
-				}
-
-				var messageEmbed = new Discord.MessageEmbed()
-					.setColor("#f347ff")
-					.setTitle("User unbanned")
-					.setThumbnail(member.user.displayAvatarURL())
-					.setDescription(`${member} was unbanned`)
-					.setTimestamp(new Date());
-
-
-				server.logChannel.send({embed: messageEmbed});
-			} catch(err){
-				console.log(err);
-			}
-		});
-
-		bot.client.on("guildMemberUpdate", (oldMember, newMember) => {
-			try{
-				let server = bot.servers[oldMember.guild.id];
-				if(server.loggingEnabled != "true"){
-					return;
-				}
-				
-				if(server.loggingUser == "true"){
-					if(oldMember.nickname != newMember.nickname){
-						var messageEmbed = new Discord.MessageEmbed()
-							.setColor("#f347ff")
-							.setTitle(newMember.toString() + " nickname's changed")
-							.setThumbnail(oldMember.user.displayAvatarURL())
-							.setDescription(`Old nickame: ${oldMember.displayName}\nNew nickname: ${newMember.displayName}`)
-							.setTimestamp(new Date());
+		//Bans
+		bot.client.on("guildBanAdd", (guild, member) => guildBanAdd(guild, member));
+		bot.client.on("guildBanRemove", (guild, member) => guildBanRemove(guild, member));
 		
-						try{
-							server.logChannel.send({embed: messageEmbed});
-						} catch(err){
-							console.log(err);
-						}
-					}
-
-					if(!oldMember.roles.equals(newMember)){
-						var rolesChanged = "";
-						if(oldMember.roles.size > newMember.roles.size){
-							oldMember.roles.forEach(function(value, key, map) {
-								if(!newMember.roles.has(key)) rolesChanged += oldMember.roles.get(key).toString() + "\n";
-							});
-
-							var messageEmbed = new Discord.MessageEmbed()
-								.setColor("#f347ff")
-								.setTitle("Took role from " + newMember.displayName)
-								.setThumbnail(oldMember.user.displayAvatarURL())
-								.setDescription(`Roles removed:\n ${rolesChanged}`)
-								.setTimestamp(new Date());
-
-							server.logChannel.send({embed: messageEmbed});
-						} else if(newMember.roles.size > oldMember.roles.size){
-							newMember.roles.forEach(function(value, key, map) {
-								if(!oldMember.roles.has(key)) rolesChanged += newMember.roles.get(key).toString() + "\n";
-							});
-
-							var messageEmbed = new Discord.MessageEmbed()
-								.setColor("#f347ff")
-								.setTitle("Gave role to " + newMember.displayName)
-								.setThumbnail(oldMember.user.displayAvatarURL())
-								.setDescription(`Roles given: ${rolesChanged}`)
-								.setTimestamp(new Date());
-
-							server.logChannel.send({embed: messageEmbed});
-						}
-					}
-				}
-			} catch(err){
-				console.log(err);
-			}
-		});
-
+		//Logging users getting roles or changed nicknames
+		bot.client.on("guildMemberUpdate", (oldMember, newMember) => guildMemberUpdate(oldMember, newMember));
+		
+/*		//Not done yet
+		//Update to channels in general
+		bot.client.on("channelCreate", channel => channelCreate(channel));
+		bot.client.on("channeleDelete", channel => channelDelete(channel));
+		bot.client.on("channelPinsUpdate", (channel, time) => channelEdit(channel, time));
+		bot.client.on("channelUpdate", (oldChannel, newChannel) => channelUpdate(oldChannel, newChannel));
+		
+		//Emojis created/removed/edited
+		bot.client.on("emojiCreate", emoji => emojiCreate(emoji));
+		bot.client.on("emojiDelete", emoji => emojiDelete(emoji));
+		bot.client.on("emojiUpdate", (oldEmoji, newEmoji) => emojiUpdate(oldEmoji, newEmoji));
+		
+		//Edits to roles within the server. Does not include giving roles to people
+		bot.client.on("roleCreate", role => roleCreate(role));
+		bot.client.on("roleDelete", role => roleDelete(role));
+		bot.client.on("roleUpdate", (oldRole, newRole) => roleUpdate(oldRole, newRole));
+*/
 	} catch(err){
 		console.log(err);
 	}
-	
-	
+		
 	function isDM(message){
 		return message.channel.type == 'dm';
+	}
+
+	/*
+	 *  Guild Member Add: Member joined the guild
+	 */
+	async function guildMemberAdd(member){
+		try{
+			let server = bot.servers[member.guild.id];
+			
+			if(server.loggingEnabled != "true" || server.loggingJoin != "true"){
+				return;
+			}
+
+			var joinEmbed = new Discord.MessageEmbed()
+				.setColor("#4760ff")
+				.setTitle("User joined")
+				.setThumbnail(member.user.displayAvatarURL())
+				.setDescription(`${member} has joined!\n${member.guild.memberCount} users in this guild.`)
+				.setTimestamp(new Date())
+				.setFooter(`User ID: ${member.id} | Username: ${member.user.username}`);
+
+
+			if(server.userLogChannel != undefined){
+				server.userLogChannel.send({embed: joinEmbed});
+			} else {
+				server.logChannel.send({embed: joinEmbed});
+			}
+		} catch(err){
+			console.log(err);
+		}
+		
+	}
+
+	/*
+	 *  Guild Member Remove: Member left the guild
+	 */
+	async function guildMemberRemove(member){
+		try{
+			let server = bot.servers[member.guild.id];
+			
+			if(server.loggingEnabled != "true" || server.loggingJoin != "true"){
+				return;
+			}
+			
+			var joinEmbed = new Discord.MessageEmbed()
+				.setColor("#4760ff")
+				.setTitle("User left")
+				.setThumbnail(member.user.displayAvatarURL())
+				.setDescription(`${member} has left!\n${member.guild.memberCount} users in this guild.`)
+				.setTimestamp(new Date())
+				.setFooter(`User ID: ${member.id} | Username: ${member.user.username}`);
+
+
+			if(server.userLogChannel != undefined){
+				server.userLogChannel.send({embed: joinEmbed});
+			} else {
+				server.logChannel.send({embed: joinEmbed});
+			}
+		} catch(err){
+			console.log(err);
+		}
+	}
+
+	/*
+	 *  Message Delete: Singular message deleted
+	 */
+	async function messageDelete(message){
+		try{
+			if(isDM(message)) return;
+			let server = bot.servers[message.guild.id];
+			if(server.loggingEnabled != "true" || server.loggingMessage != "true"){
+				return;
+			}
+
+
+			var messageEmbed = new Discord.MessageEmbed()
+				.setColor("#ff4747")
+				.setTitle("Message by <unknown> user deleted")
+				.setDescription(`Message content:\n${message.content}`)
+				.setTimestamp(new Date())
+			
+			if(message.member != undefined){
+				messageEmbed.setTitle("Message by " + message.member.displayName + " deleted")
+				.setThumbnail(message.member.user.displayAvatarURL())
+				.setFooter(`User ID: ${message.member.id} | Username: ${message.member.user.username}`);
+			}
+
+
+			server.logChannel.send({embed: messageEmbed});
+		} catch(err){
+			console.log(err);
+		}
+	}
+
+	/*
+	 *  Message Delete Bulk: Bulk delete of messages
+	 */
+	async function messageDeleteBulk(message){
+		try{
+			if(isDM(message)) return;
+			let server = bot.servers[message.first().guild.id];
+			if(server.loggingEnabled != "true" || server.loggingMessage != "true"){
+				return;
+			}
+
+			var messageEmbed = new Discord.MessageEmbed()
+				.setColor("#ff4747")
+				.setTitle("Messages deleted in bulk")
+				.setDescription(`${message.size} messages deleted in ${message.first().channel}`)
+				.setTimestamp(new Date());
+
+			server.logChannel.send({embed: messageEmbed});
+		} catch(err){
+			console.log(err);
+		}
+	}
+
+	/*
+	 *  Message Update: When a message has been changed
+	 */
+	async function messageUpdate(oldMessage, newMessage){
+		try{
+			if(isDM(oldMessage)) return;
+			var server = null;
+
+			server = bot.servers[oldMessage.guild.id];
+
+			if(server.loggingEnabled != "true" || server.loggingMessage != "true"){
+				return;
+			}
+
+			if(oldMessage.content == newMessage.content){
+				return;
+			}
+
+			var messageEmbed = new Discord.MessageEmbed()
+				.setColor("#ff4747")
+				.setTitle("Messages edited by " + oldMessage.member.displayName)
+				.setThumbnail(oldMessage.member.user.displayAvatarURL())
+				.setDescription(`Old message:\`\`\`\n${oldMessage.content}\`\`\`\nNew message:\`\`\`\n${newMessage.content}\`\`\``)
+				.setTimestamp(new Date())
+				.setFooter(`User ID: ${oldMessage.member.id} | Username: ${oldMessage.member.user.username}`);
+
+
+			server.logChannel.send({embed: messageEmbed});
+		} catch(err){
+			console.log(err);
+		}
+	}
+
+    /*
+	 *  Guild Ban Add: Fired when a guild member is banned  
+	 */
+	async function guildBanAdd(guild, member){
+		try{
+			let server = bot.servers[guild.id];
+			if(server.loggingEnabled != "true" || server.loggingUser != "true"){
+				return;
+			}
+
+			var messageEmbed = new Discord.MessageEmbed()
+				.setColor("#f347ff")
+				.setTitle("User banned")
+				.setThumbnail(member.user.displayAvatarURL())
+				.setDescription(`${member} was banned`)
+				.setTimestamp(new Date())
+				.setFooter(`User ID: ${member.id} | Username: ${member.user.username}`);
+				
+
+			server.logChannel.send({embed: messageEmbed});
+		} catch(err){
+			console.log(err);
+		}
+	}
+
+    /*
+	 *  Guild Ban Remove: Fired when a guild member is unbanned  
+	 */
+	async function guildBanRemove(guild, member){
+		try{
+			let server = bot.servers[guild.id];
+			if(server.loggingEnabled != "true" || server.loggingUser != "true"){
+				return;
+			}
+
+			var messageEmbed = new Discord.MessageEmbed()
+				.setColor("#f347ff")
+				.setTitle("User unbanned")
+				.setThumbnail(member.user.displayAvatarURL())
+				.setDescription(`${member} was unbanned`)
+				.setTimestamp(new Date())
+				.setFooter(`User ID: ${member.id} | Username: ${member.user.username}`);
+
+
+			server.logChannel.send({embed: messageEmbed});
+		} catch(err){
+			console.log(err);
+		}
+	}
+
+    /*
+	 *  Guild Member Update: Roles and nickname changes
+	 */
+	async function guildMemberUpdate(oldMember, newMember){
+		try{
+			let server = bot.servers[oldMember.guild.id];
+			if(server.loggingEnabled != "true"){
+				return;
+			}
+			
+			if(server.loggingUser == "true"){
+				if(oldMember.nickname != newMember.nickname){
+					var messageEmbed = new Discord.MessageEmbed()
+						.setColor("#f347ff")
+						.setTitle(newMember.toString() + " nickname's changed")
+						.setThumbnail(oldMember.user.displayAvatarURL())
+						.setDescription(`Old nickame: ${oldMember.displayName}\nNew nickname: ${newMember.displayName}`)
+						.setTimestamp(new Date())
+						.setFooter(`User ID: ${oldMember.id} | Username: ${oldMember.user.username}`);
+	
+					try{
+						server.logChannel.send({embed: messageEmbed});
+					} catch(err){
+						console.log(err);
+					}
+				}
+
+				if(!oldMember.roles.equals(newMember)){
+					var rolesChanged = "";
+					if(oldMember.roles.size > newMember.roles.size){
+						oldMember.roles.forEach(function(value, key, map) {
+							if(!newMember.roles.has(key)) rolesChanged += oldMember.roles.get(key).toString() + "\n";
+						});
+
+						var messageEmbed = new Discord.MessageEmbed()
+							.setColor("#f347ff")
+							.setTitle("Took role from " + newMember.displayName)
+							.setThumbnail(oldMember.user.displayAvatarURL())
+							.setDescription(`Roles removed:\n ${rolesChanged}`)
+							.setTimestamp(new Date())
+							.setFooter(`User ID: ${oldMember.id} | Username: ${oldMember.user.username}`);
+
+						server.logChannel.send({embed: messageEmbed});
+					} else if(newMember.roles.size > oldMember.roles.size){
+						newMember.roles.forEach(function(value, key, map) {
+							if(!oldMember.roles.has(key)) rolesChanged += newMember.roles.get(key).toString() + "\n";
+						});
+
+						var messageEmbed = new Discord.MessageEmbed()
+							.setColor("#f347ff")
+							.setTitle("Gave role to " + newMember.displayName)
+							.setThumbnail(oldMember.user.displayAvatarURL())
+							.setDescription(`Roles given: ${rolesChanged}`)
+							.setTimestamp(new Date())
+							.setFooter(`User ID: ${oldMember.id} | Username: ${oldMember.user.username}`);
+
+						server.logChannel.send({embed: messageEmbed});
+					}
+				}
+			}
+		} catch(err){
+			console.log(err);
+		}
 	}
 	//User Welcome/Leave
 }
---
+++ modules/skillpoints.js
@@ -0,0 +1,136 @@
+const Discord = require('discord.js');
+
+var statEnum = {
+    HP: "HP",
+    Speed: "SPEED",
+    Damage: "DAMAGE",
+    EXP: "EXP"
+}
+
+module.exports = {
+    name: "skillpoints",
+    aliases: ["sp"],    
+    helpDesc: "Rank up/View stats for fighting",
+    helpTitle: "SkillPoints",
+    run: async (bot, message, args) => {
+        let server = bot.servers[message.guild.id];
+        var embed = new Discord.MessageEmbed();
+        let levelUtil = require(`../utils/fightLvl.js`);
+        var skillPts;
+        var num;
+        var statChanged;
+        var newTotal;
+
+        var person = message.mentions.members.first() ? message.mentions.members.first() : message.member;
+
+        bot.fightDB.get("SELECT * from ServerID" + message.guild.id + " WHERE userID=" + person.id, async function(err, row){
+            if(err){
+                console.log(err);
+                return;
+            }
+
+            if(row == undefined){
+                sqlDataPer1 = await levelUtil.newMember(person);
+            } else {
+                sqlDataPer1 = row;
+            }
+
+            sqlDataPer1.level = await levelUtil.calcLevel(sqlDataPer1.EXP)
+            
+            expForm = {
+                currentEXP: 0,
+                neededEXP: 0
+            }
+
+            await levelUtil.neededEXP(sqlDataPer1.EXP, expForm);
+
+            //Skillpoints left to spend
+            skillPts = parseInt(sqlDataPer1.level) - (parseInt(sqlDataPer1.HP) + parseInt(sqlDataPer1.SPEED) + parseInt(sqlDataPer1.DAMAGE) + parseInt(sqlDataPer1.EXPM)) - 1;
+
+            if(!args[1] || person != message.member){
+                embed.setAuthor(person.displayName + "'s stats in " + message.guild.name , person.user.displayAvatarURL());
+                embed.setDescription("Level: " + sqlDataPer1.level + "\nEXP: (" + expForm.currentEXP + " / " + expForm.neededEXP + ")");
+                embed.addField("Stat Levels", `HP: ${(sqlDataPer1.HP)}\nSpeed: ${(sqlDataPer1.SPEED)}\nDamage: ${(sqlDataPer1.DAMAGE)}\nEXP Multiplier: ${(sqlDataPer1.EXPM)}`);
+                embed.addField("Skill points to spend", skillPts + "\nSpend by doing " + server.prefix + "skillpoints <name> <num of points to spend>");
+            } else {
+                embed.setAuthor(person.displayName + " Skill Point Confirmation", person.user.displayAvatarURL());;
+                statChanged;
+                num = 1;
+
+                //Check if user wants to spend more than one 
+                if(args[2]){
+                    try{
+                        num = parseInt(args[2]);
+                        if(num < 1) throw "Less than 1";
+                    } catch (err) {
+                        return message.channel.send("Not a valid number! Err: `" + err + "`");
+                    }
+                }
+
+                switch(args[1].toUpperCase()){
+                    case statEnum.HP:
+                        statChanged = statEnum.HP;
+                        newTotal = sqlDataPer1.HP + num;
+                        break;
+                    case statEnum.EXP:
+                        statChanged = statEnum.EXP;
+                        newTotal = sqlDataPer1.EXPM + num;
+                        break;
+                    case statEnum.Damage:
+                        statChanged = statEnum.Damage;
+                        newTotal = sqlDataPer1.DAMAGE + num;
+                        break;
+                    case statEnum.Speed:
+                        statChanged = statEnum.Speed;
+                        newTotal = sqlDataPer1.SPEED + num;
+                        break;
+                    default:
+                        return message.channel.send("Not a valid stat!");
+                }
+
+                //Check that you have enough skill pts
+                if(num > skillPts){
+                    return message.channel.send("You don't have that many skill points to spend! You have `" + skillPts + "` points to spend.");
+                }
+
+                embed.setDescription("Do you really want to spend " + num + " upgrading " + statChanged + "?\nRespond with `Yes` or `No`");
+
+                message.collector = message.channel.createMessageCollector(m => m.member.id == message.member.id, { time: 60000 });
+                message.collector.on('collect', m => handleAnswer(m));
+                message.collector.on('end', reason => handleEnd(reason));
+            }
+
+            message.channel.send({embed: embed});
+
+        });
+
+        function handleEnd(reason){
+            if(reason != 'Answer Given'){
+                message.channel.send("No answer given before time-out. Please re-run the command.");
+            }
+        }
+
+        async function handleAnswer(m){
+            var answerEmbed = new Discord.MessageEmbed();
+            answerEmbed.setAuthor(person.displayName + " Skill Point Confirmation" , person.user.displayAvatarURL());
+
+            if(m.content.toLowerCase() == "yes"){
+                answerEmbed.setDescription("Putting `" + num + "` points into `" + statChanged + "`");
+                
+                //Different name in SQL
+                if(statChanged == statEnum.EXP) statChanged = "EXPM";
+
+                bot.fightDB.run("UPDATE ServerID" + message.guild.id + " SET " + statChanged + " = " + newTotal + " WHERE userID=" + person.id, err => {
+                    if(err) message.channel.send("Error occured putting data into Database!\nError Message: `" + err + "`");
+                });
+            } else if (m.content.toLowerCase() == "no"){
+                answerEmbed.setDescription("Cancelling...");
+            } else {
+                return;
+            }
+
+            message.channel.send({embed: answerEmbed})
+            message.collector.stop('Answer Given');
+        }
+    }
+} 
